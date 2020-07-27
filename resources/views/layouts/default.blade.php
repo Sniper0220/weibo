668645b5fcd18b10b77aa63d797fbc7abb80d1bc
liuzhaolun <1132559553@qq.com>
2020-07-27 14:48:45 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-  </head>
-  <body>
-	@include('layouts._header')
-    <div class="container">
-      @yield('content')
-      @include('layouts._footer')
-    </div>
-  </body>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+@include('layouts._header')
+<div class="container">
+    @include('shared._messages')
+    @yield('content')
+    @include('layouts._footer')
+</div>
+</body>
 </html>
