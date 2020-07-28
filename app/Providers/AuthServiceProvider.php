35e49098e9c5df04674980b676534a0ee5fe3248
liuzhaolun <1132559553@qq.com>
2020-07-28 17:38:23 +0800

    删除微博

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,7 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         Gate::guessPolicyNamesUsing(function ($modelClass) {
             // 动态返回模型对应的策略名称，如：// 'App\Models\User' => 'App\Policies\UserPolicy',
             return 'App\Policies\\'.class_basename($modelClass).'Policy';
-        });
+    });
         //
     }
 }
