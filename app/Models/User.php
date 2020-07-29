9dd5c9ab85da79d795226c7f08a041857e5f8a2e
liuzhaolun <1132559553@qq.com>
2020-07-29 09:31:52 +0800

    微博列表增加关注人的列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,7 +59,10 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
             ->orderBy('created_at', 'desc');
     }
 
@@ -88,6 +91,7 @@ class User extends Authenticatable
         }
         $this->followings()->detach($user_ids);
     }
+
     public function isFollowing($user_id)
     {
         return $this->followings->contains($user_id);
